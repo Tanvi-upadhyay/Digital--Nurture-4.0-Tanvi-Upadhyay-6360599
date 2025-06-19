3afb5b14096cfd2501c7853f943597df22f7ce6d
Tanvi Upadhyay <118131005+Tanvi-upadhyay@users.noreply.github.com>
2025-06-19 23:13:52 +0530

    Add files via upload

---
+++ DPP(Java)/BuilderPatternExample/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Computer gamingPC = new Computer.Builder()
+            .setCPU("Intel i9")
+            .setRAM("32GB")
+            .setStorage("1TB SSD")
+            .build();
+
+        Computer officePC = new Computer.Builder()
+            .setCPU("Intel i5")
+            .setRAM("16GB")
+            .setStorage("512GB SSD")
+            .build();
+
+        gamingPC.displayConfig();
+        officePC.displayConfig();
+    }
+}
