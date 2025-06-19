9d1eb3fe44ae54d868e4b5b567b623c567751c84
Tanvi Upadhyay <118131005+Tanvi-upadhyay@users.noreply.github.com>
2025-06-19 23:04:31 +0530

    Delete DSA directory

--- DSA/EmployeeManagementSystem/EmployeeManagementSystem/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        EmployeeManager manager = new EmployeeManager();
-        manager.addEmployee(new Employee("E001", "John", "Developer", 60000));
-        manager.addEmployee(new Employee("E002", "Jane", "Manager", 75000));
-        manager.traverseEmployees();
-    }
-}
--- DSA/InventoryManagementSystem/InventoryManagementSystem/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        InventoryManager manager = new InventoryManager();
-        Product p1 = new Product("P001", "Laptop", 10, 75000);
-        manager.addProduct(p1);
-        System.out.println("Product added: " + p1.getProductId());
-    }
-}
