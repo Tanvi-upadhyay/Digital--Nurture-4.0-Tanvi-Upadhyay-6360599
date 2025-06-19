76f65279d8084670e6d94504981acfbb050c2c54
Tanvi Upadhyay <118131005+Tanvi-upadhyay@users.noreply.github.com>
2025-06-19 22:50:16 +0530

    Add files via upload
    Revised Mandatory Hands-On In .Net

---
+++ Data structures and Algorithms/FinancialForecastingExample/FinancialForecastingExample/Forecast.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Forecast
+{
+  // Recursive method to predict future value without memoization
+  public static double PredictValue(double[] data, int years)
+  {
+    if (years == 0)
+      return data[data.Length - 1];
+
+    double lastValue = data[data.Length - 1];
+    double secondLastValue = data[data.Length - 2];
+    double growthRate = (lastValue - secondLastValue) / secondLastValue;
+
+    return PredictValue(data, years - 1) * (1 + growthRate);
+  }
+
+  // Optimized with memoization
+  public static double PredictValueMemoized(double[] data, int years, double[] memo)
+  {
+    if (memo[years] != 0)
+      return memo[years];
+
+    if (years == 0)
+      return data[data.Length - 1];
+
+    double lastValue = data[data.Length - 1];
+    double secondLastValue = data[data.Length - 2];
+    double growthRate = (lastValue - secondLastValue) / secondLastValue;
+
+    memo[years] = PredictValueMemoized(data, years - 1, memo) * (1 + growthRate);
+    return memo[years];
+  }
+}
