d8d680bae1bc025f619b2ca4946c4d634ac5df04
Tanvi Upadhyay <118131005+Tanvi-upadhyay@users.noreply.github.com>
2025-06-19 22:59:17 +0530

    Add files via upload

---
+++ Design Patterns and Principles(.Net)/FactoryMethodPatternExample/FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    DocumentFactory factory;
+
+    factory = new WordDocumentFactory();
+    IDocument doc1 = factory.CreateDocument();
+    doc1.Open();
+
+    factory = new PdfDocumentFactory();
+    IDocument doc2 = factory.CreateDocument();
+    doc2.Open();
+
+    factory = new ExcelDocumentFactory();
+    IDocument doc3 = factory.CreateDocument();
+    doc3.Open();
+  }
+}
---
+++ Design Patterns and Principles(.Net)/SingletonPatternExample/SingletonPatternExample/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Logger logger1 = Logger.GetInstance();
+    Logger logger2 = Logger.GetInstance();
+
+    logger1.Log("This is the first log message.");
+    logger2.Log("This is the second log message.");
+
+    Console.WriteLine(Object.ReferenceEquals(logger1, logger2)
+        ? "Only one instance exists."
+        : "Different instances exist.");
+  }
+}
